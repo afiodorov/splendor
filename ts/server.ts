eed5875b09fee0457e33a2717a1473da77bc0a05
Tom Artiom Fiodorov <a.fiodorov@yahoo.co.uk>
2015-05-26 22:51:39 +0100

    router thing removed

--- ts/server.ts
+++ ts/server.ts
@@ -10,23 +10,9 @@ app.engine('jade', require('jade').__express)
 app.set('views', __dirname + '/../views/')
 app.set('view engine', 'jade');
 
-var router = express.Router();
-
-router.use((req, res, next) => { next(); })
-router.route('/users')
-    .get((req, res, next) => {
-        res.send(req.query['token']);
-    });
-
-app.use((req, res, next) => {
-    // hacky trick, router is just a handler
-    router(req, res, next);
-});
-
 app.get('/', function(req, res) {
 	res.render('game');
 });
 
-app.use(router);
 app.use(express.static(__dirname + '/../public/'));
 app.listen(port)
